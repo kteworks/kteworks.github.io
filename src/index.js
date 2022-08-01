0959ca6816f306891dc0266d7761758f00bf7115
kteworks <110329418+kteworks@users.noreply.github.com>
2022-08-02 07:49:51 +0900

    import bootstrap

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
