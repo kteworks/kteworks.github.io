02e64d992a2c1bdfd560340ecbcae9108c95c525
kteworks <110329418+kteworks@users.noreply.github.com>
2022-08-03 01:00:00 +0900

    update profile

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Button } from 'react-bootstrap';
-import reactImage from "../Images/react.png";
-import jsImage from "../Images/js.png";
-import firebaseImage from "../Images/firebase.jpg";
+import clangImage from "../Images/c-programming-logo.png";
+import javaImage from "../Images/java-logo.png";
+import csharpImage from "../Images/c-sharp-logo.png";
 import profileImage from "../Images/user_icon.png";
+import {Link} from "react-router-dom"
 
 const Homepage = () => {
   return (
@@ -13,7 +13,9 @@ const Homepage = () => {
       <img src={profileImage} className="profileImage" />
 
       <p>
-        職業訓練にてC言語/Java/JSP/Servlet/SQLの基礎を学びました。趣味でも時々プログラミングを行っています。
+        職業訓練にてC言語/Java/JSP/Servlet/C#/Unityの基礎を学びました。
+        あらゆることに好奇心旺盛で深く広く学びたいです。気づいたらブラウザのタブが増えています。
+        こちらのページはReactで作成しています。
       </p>
 
       <section class="page-section" id="services">
@@ -26,39 +28,47 @@ const Homepage = () => {
           </div>
           <div class="row text-center">
             <div class="col-md-4">
+            <a href="https://momotarorpg.herokuapp.com" target="_blank" rel="noopener noreferrer">
               <span class="fa-stack fa-4x">
+                
                 <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                <i class="fas fa-gamepad fa-stack-1x fa-inverse"></i>
+                
               </span>
-              <h4 class="my-3">ダミー1</h4>
+              <h4 class="my-3">ももたろうRPG</h4>
+              </a>
               <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
+                職業訓練のグループ課題で作成したブラウザゲームです。JSP/Servletで
+                作成しました。<br />現在、SQLデータベース移行中のため動作しません。
               </p>
             </div>
             <div class="col-md-4">
+              <a href="https://github.com/kteworks/H2DB-to-SQL" target="_blank" rel="noopener noreferrer">
               <span class="fa-stack fa-4x">
                 <i class="fas fa-circle fa-stack-2x text-primary"></i>
                 <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
               </span>
-              <h4 class="my-3">ダミー2</h4>
+              <h4 class="my-3">H2DB-to-SQL</h4>
+              </a>
               <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
+                現在制作途中。自分用にH2データベースからPostgreSQLに移行するためのアプリケーションを
+                制作しています。自分用なので今後の更新、他のデータベースへの対応、例外処理等を行うかは
+                未定です。
               </p>
             </div>
             <div class="col-md-4">
+            <a href="https://github.com/kteworks/Breakout-Game" target="_blank" rel="noopener noreferrer">
               <span class="fa-stack fa-4x">
+              
                 <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+                <i class="fas fa-cube fa-stack-1x fa-inverse"></i>
+                
               </span>
-              <h4 class="my-3">ダミー3</h4>
+              <h4 class="my-3">Breakout</h4>
+              </a>
               <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
+                Unityで制作した単純なブロックくずしゲームです。不具合のような仕様も楽しめてもらえるかと思います。
+                ドット絵画像を読み込んでブロック生成できたりするのが特徴です。Windows専用。<a href="https://github.com/kteworks/Breakout-Game/releases/"  target="_blank" rel="noopener noreferrer">最新リリース</a>
               </p>
             </div>
           </div>
@@ -70,23 +80,23 @@ const Homepage = () => {
           <h1 class="title">スキル</h1>
           <div class="row text-center">
             <div class="col-md-4 services">
-              <img src={reactImage} />
-              <h4>React</h4>
-              <p>Reactがつかえます</p>
+              <img src={clangImage} />
+              <h4>C Programming Language</h4>
+              <p>C言語がつかえます</p>
             </div>
             <div class="col-md-4 services">
-              <img src={jsImage} />
-              <h4>HTML/CSS</h4>
-              <p>HTML/CSSがつかえます</p>
+              <img src={javaImage} />
+              <h4>Java</h4>
+              <p>Java/Servletがつかえます</p>
             </div>
             <div class="col-md-4 services">
-              <img src={firebaseImage} />
-              <h4>Firebase</h4>
-              <p>Firebaseがつかえます</p>
+              <img src={csharpImage} />
+              <h4>C#</h4>
+              <p>.NET Frameworkがつかえます</p>
             </div>
           </div>
           <button type="button" class="btn btn-primary">
-            スキル一覧
+            スキル一覧(工事中)
           </button>
         </div>
       </section>
---
+++ src/components/dummy.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div className="container">DummyPage
+    <h1>ブログ</h1>
+      
+      <p>aaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaa
+      </p>
+    </div>
+  )
+}
+
+export default BlogPage
